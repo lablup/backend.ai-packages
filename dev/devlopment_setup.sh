9af8cee3cbd3943749f2f1fc8e2694b855a39391
Seongyoung-Kang <ksy9164@naver.com>
2019-01-24 11:02:57 +0900

    add directory for develeopment setup

---
+++ dev/devlopment_setup.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+# sudo apt install curl -y
+#
+# # Docker
+# curl -fsSL https://get.docker.com/ | sudo sh
+# sudo addgroup --system docker
+# sudo adduser $USER docker
+# newgrp docker
+# sudo usermod -aG docker $USER
+#
+# # Docker Compose
+# sudo apt install docker-compose
+# docker-compose -f docker-compose.halfstack.yml up -d
+# docker pull lablup/kernel-python:3.6-debian
+# sudo docker pull lablup/kernel-python:3.6-ubuntu
+# sudo docker pull lablup/kernel-python-tensorflow:1.12-py36
+# if [ $ENABLE_CUDA -eq 1 ]; then
+#   sudo docker pull lablup/kernel-python-tensorflow:1.12-py36-cuda9
+# fi
+
+# goto manager
+# cp sample-configs/image-metadata.yml image-metadata.yml
+# cp sample-configs/image-aliases.yml image-aliases.yml
+./scripts/run-with-halfstack.sh python3 -m ai.backend.manager.cli etcd update-images -f image-metadata.yml
+./scripts/run-with-halfstack.sh python3 -m ai.backend.manager.cli etcd update-aliases -f image-aliases.yml
+
+cp alembic.ini.sample alembic.ini
+
+python3 -m ai.backend.manager.cli schema oneshot head
+python3 -m ai.backend.manager.cli --db-addr=localhost:8100 --db-user=postgres --db-password=develove --db-name=backend fixture populate sample-configs/example-keypairs.json
+
+# vfolder
+mkdir ./vfolder
+./scripts/run-with-halfstack.sh python3 -m ai.backend.manager.cli etcd put volumes/_mount "./vfolder"
+./scripts/run-with-halfstack.sh python3 -m ai.backend.manager.cli etcd put volumes/_default_host "local"
+
+# key setting
+
+export BACKEND_ENDPOINT=http://127.0.0.1:8081/
+export BACKEND_ACCESS_KEY=AKIAIOSFODNN7EXAMPLE
+export BACKEND_SECRET_KEY=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY
+
+mkdir scratches
+
