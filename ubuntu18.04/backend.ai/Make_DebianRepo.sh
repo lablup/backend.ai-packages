0159408ec9734c14c24fdf43f51853261b48b9fa
Seongyoung-Kang <ksy9164@naver.com>
2019-01-24 17:51:38 +0900

    Add union Repository

---
+++ ubuntu18.04/backend.ai/Make_DebianRepo.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+override=./conf/override.bionic
+
+touch $override
+
+# add overriding
+
+pre_depend=$(ls ./packages/pre_depend_packages)
+depend=$(ls ./packages/depend_packages)
+target=$(ls ./packages | grep "deb")
+
+for i in $pre_depend ; do
+    sudo reprepro includedeb bionic ./packages/pre_depend_packages/$i
+done
+
+for i in $depend ; do
+    sudo reprepro includedeb bionic ./packages/depend_packages/$i  
+done
+
+for i in $target ; do
+    sudo reprepro includedeb bionic ./packages/$i  
+done
