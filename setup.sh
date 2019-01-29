a7fbbe77d07d329787a5af3dbde067d1e7da5ded
Seongyoung-Kang <ksy9164@naver.com>
2019-01-29 17:42:13 +0900

    Add script for checking etc/apt/sources.list (when sources.list has our repository's path, then do not add path)

--- setup.sh
+++ setup.sh
@@ -4,9 +4,30 @@
 cat /usr/local/backend.ai/backend.ai.gpg.key | sudo apt-key add -
 
 # Add sources.list
-sudo echo "deb file:///usr/local/backend.ai/ubuntu18.04/backend.ai-client bionic main" >> /etc/apt/sources.list
-sudo echo "deb file:///usr/local/backend.ai/ubuntu18.04/backend.ai-common bionic main">> /etc/apt/sources.list
-sudo echo "deb file:///usr/local/backend.ai/ubuntu18.04/backend.ai-manager bionic main" >> /etc/apt/sources.list
-sudo echo "deb file:///usr/local/backend.ai/ubuntu18.04/backend.ai-agent bionic main" >> /etc/apt/sources.list
+
+add_client_apt='deb file:///usr/local/backend.ai/ubuntu18.04/backend.ai-client bionic main'
+add_common_apt='deb file:///usr/local/backend.ai/ubuntu18.04/backend.ai-common bionic main'
+add_manager_apt='deb file:///usr/local/backend.ai/ubuntu18.04/backend.ai-manager bionic main'
+add_agent_apt='deb file:///usr/local/backend.ai/ubuntu18.04/backend.ai-agent bionic main'
+
+apt_sources_path='/etc/apt/sources.list'
+
+apt_sources_list=$(cat $apt_sources_path)
+
+if [[ $apt_sources_list != *"$add_client_apt"* ]]; then
+    sudo echo "$add_client_apt" >> $apt_sources_path
+fi
+
+if [[ $apt_sources_list != *"$add_common_apt"* ]]; then
+    sudo echo "$add_common_apt" >> $apt_sources_path
+fi
+
+if [[ $apt_sources_list != *"$add_manager_apt"* ]]; then
+    sudo echo "$add_manager_apt" >> $apt_sources_path
+fi
+
+if [[ $apt_sources_list != *"$add_agent_apt"* ]]; then
+    sudo echo "$add_agent_apt" >> $apt_sources_path
+fi
 
 sudo apt update
