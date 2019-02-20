066c5888b2c13aeb41164a856815d33de516ea2e
Seongyoung-Kang <ksy9164@naver.com>
2019-02-20 16:37:43 +0900

    Add Curl depend

--- git-lfs_install.sh
+++ git-lfs_install.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+if ! type "curl" >/dev/null 2>&1; then
+    sudo apt install -y curl
+fi
+
 sudo apt-get install software-properties-common
 sudo add-apt-repository ppa:git-core/ppa
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
